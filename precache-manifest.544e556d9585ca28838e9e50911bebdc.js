ed05134d58a22ba0c95679ffb78f5b3d66445f75
duvld <ollejna@gmail.com>
2020-06-13 15:27:03 -0400

    Updates

---
+++ precache-manifest.544e556d9585ca28838e9e50911bebdc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9922b0a62ab3244fd28b36e04753d209",
+    "url": "/react-portfolio-test/index.html"
+  },
+  {
+    "revision": "86618433e0f7a38eb1d3",
+    "url": "/react-portfolio-test/static/css/main.43d02339.chunk.css"
+  },
+  {
+    "revision": "75697d0972472b015d81",
+    "url": "/react-portfolio-test/static/js/2.f98ac5ed.chunk.js"
+  },
+  {
+    "revision": "86618433e0f7a38eb1d3",
+    "url": "/react-portfolio-test/static/js/main.2e1f955d.chunk.js"
+  },
+  {
+    "revision": "7e3b0e09fd7561ebd8df",
+    "url": "/react-portfolio-test/static/js/runtime-main.44f56dc0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/react-portfolio-test/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/react-portfolio-test/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/react-portfolio-test/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/react-portfolio-test/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/react-portfolio-test/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-portfolio-test/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/react-portfolio-test/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/react-portfolio-test/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/react-portfolio-test/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
